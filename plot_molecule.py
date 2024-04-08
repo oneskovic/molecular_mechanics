1baf580d1c64ff6fe5326b8bddc651474769461f
Ognjen <oneskovic0@gmail.com>
2024-04-08 23:52:53 +0200

    Test DNA plot

---
+++ plot_molecule.py
@@ -0,0 +1,14 @@
+from xyz2graph import MolGraph, to_networkx_graph, to_plotly_figure
+from plotly.offline import offline
+
+# Create the MolGraph object
+mg = MolGraph()
+
+# Read the data from the .xyz file
+mg.read_xyz('dna.xyz')
+
+# Create the Plotly figure object
+fig = to_plotly_figure(mg)
+
+# Plot the figure
+offline.plot(fig)
