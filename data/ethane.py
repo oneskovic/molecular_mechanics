4ea601de1fc9a77df23aa868c21c3c8b84fc7e79
PavleSekesan <pavlesekesan@gmail.com>
2024-04-29 01:57:45 +0200

    main.py structured as cli application

---
+++ data/ethane.py
@@ -0,0 +1,41 @@
+import torch
+from molecular_mechanics.atom import Atom
+from molecular_mechanics.forces import DihedralForce, DihedralForceParams, ForceField, HarmonicAngleForce, HarmonicAngleForceParams, HarmonicBondForce, HarmonicBondForceParams
+
+
+atoms = [
+    Atom("H", torch.tensor(
+        [1.1851, -0.0039, 0.9875], requires_grad=True), 1.0),
+    Atom("C", torch.tensor(
+        [0.7516, -0.0225, -0.0209], requires_grad=True), 12.0),
+    Atom("H", torch.tensor(
+        [1.1669, 0.8330, -0.5693], requires_grad=True), 1.0),
+    Atom("H", torch.tensor(
+        [1.1155, -0.9329, -0.5145], requires_grad=True), 1.0),
+    Atom("C", torch.tensor(
+        [-0.7516, 0.0225, 0.0209], requires_grad=True), 12.0),
+    Atom("H", torch.tensor(
+        [-1.1669, -0.8334, 0.5687], requires_grad=True), 1.0),
+    Atom("H", torch.tensor(
+        [-1.1157, 0.9326, 0.5151], requires_grad=True), 1.0),
+    Atom("H", torch.tensor(
+        [-1.1850, 0.0044, -0.9875], requires_grad=True), 1.0),
+]
+force_field = ForceField(
+    harmonic_bond_forces=HarmonicBondForce(
+        {
+            ("C", "C"): HarmonicBondForceParams(1.5375, 251),
+            ("C", "H"): HarmonicBondForceParams(1.0949, 278),
+        }
+    ),
+    harmonic_angle_forces=HarmonicAngleForce(
+        {
+            ("H", "C", "C"): HarmonicAngleForceParams(1.912, 388),
+            ("H", "C", "H"): HarmonicAngleForceParams(1.892, 328),
+        }
+    ),
+    dihedral_forces=DihedralForce(
+        {("H", "C", "C", "H"): DihedralForceParams(0.6276, 3, 0.0)}
+    )
+)
+connections = [[1], [0, 2, 3, 4], [1], [1], [1, 5, 6, 7], [4], [4], [4]]
--- molecular_mechanics/main.py
+++ molecular_mechanics/main.py
@@ -1,41 +1,20 @@
-import torch
+import argparse
+import importlib.util
+import pathlib
+
 from torch.optim import Adam
 from matplotlib import pyplot as plt
 
-from molecular_mechanics.atom import Atom, get_bond_angle, get_distance
-from molecular_mechanics.forces import (
-    ForceField,
-    HarmonicAngleForce,
-    HarmonicAngleForceParams,
-    HarmonicBondForce,
-    HarmonicBondForceParams,
-)
 from molecular_mechanics.integration import VerletIntegrator
 from molecular_mechanics.logging import XYZTrajectoryWriter, print_system_state
 from molecular_mechanics.system import System
 
-atoms = [
-    Atom("H", torch.tensor([0.0, 0.9572, 0.0], requires_grad=True), 1.0),
-    Atom("O", torch.tensor([0.0, 0.0, 0.0], requires_grad=True), 16.0),
-    Atom("H", torch.tensor([0.0, 0.0, 0.9572], requires_grad=True), 1.0),
-]
-force_field = ForceField(
-    harmonic_bond_forces=HarmonicBondForce(
-        {("O", "H"): HarmonicBondForceParams(0.9572, 553.0)}
-    ),
-    harmonic_angle_forces=HarmonicAngleForce(
-        {("H", "O", "H"): HarmonicAngleForceParams(1.82421813418, 100)}
-    )
-)
-connections = [[1], [0, 2], [1]]
-system = System(atoms, connections, force_field, temperature=290.0)
 
-def minimize_energy(system: System):
+def minimize_energy(system: System, max_iterations: int = 1000):
     positions = [atom.position for atom in system.atoms]
     optimizer = Adam(positions)
 
-    i = 0
-    while True:
+    for i in range(max_iterations):
         if i % 100 == 0:
             print(f"Iteration {i}")
             print_system_state(system)
@@ -46,54 +25,76 @@ def minimize_energy(system: System):
         optimizer.step()
         optimizer.zero_grad()
 
-def dynamics(system: System, iterations: int):
+def run_dynamics(system: System, iterations: int, trajectory_file: str, verbose: bool = False, energy_plot_file: str | None = None):
     integrator = VerletIntegrator(system)
     print_freq = 100
     sample_freq = 20
     total_energy = []
     potential_energy = []
     kinetic_energy = []
-    bond_angle = []
-    bond_length12 = []
-    bond_length23 = []
-    xyz_writer = XYZTrajectoryWriter("trajectory.xyz", system)
+    xyz_writer = XYZTrajectoryWriter(trajectory_file, system)
     for i in range(iterations):
-        p = system.get_potential_energy()
-        k = system.get_kinetic_energy()
-        total_energy.append((p + k).item())
-        potential_energy.append(p.item())
-        kinetic_energy.append(k.item())
-        bond_angle.append(get_bond_angle(*system.atoms).item())
-        bond_length12.append(get_distance(system.atoms[0], system.atoms[1]).item())
-        bond_length23.append(get_distance(system.atoms[1], system.atoms[2]).item())
-        if i % sample_freq == 0:
-            xyz_writer.write()
-        if i % print_freq == 0:
+        if energy_plot_file:
+            p = system.get_potential_energy()
+            k = system.get_kinetic_energy()
+            total_energy.append((p + k).item())
+            potential_energy.append(p.item())
+            kinetic_energy.append(k.item())
+        
+        if verbose and i % print_freq == 0:
             print(f"Iteration {i}")
             print_system_state(system)
+
+        if i % sample_freq == 0:
+            xyz_writer.write()
         integrator.step()
     xyz_writer.close()
 
-    fig, axs = plt.subplot_mosaic([
-        ["energy", "energy"],
-        ["bond_angle", "bond_lengths"]
-    ])
-
-    axs["energy"].set_title("Energies")
-    axs["energy"].plot(total_energy, label="Total Energy")
-    axs["energy"].plot(potential_energy, label="Potential Energy")
-    axs["energy"].plot(kinetic_energy, label="Kinetic Energy")
-    axs["energy"].legend()
-
-    axs["bond_angle"].set_title("Bond Angle")
-    axs["bond_angle"].plot(bond_angle, label="Bond Angle")
-
-    axs["bond_lengths"].set_title("Bond Lengths")
-    axs["bond_lengths"].plot(bond_length12, label="Bond Length 1-2")
-    axs["bond_lengths"].plot(bond_length23, label="Bond Length 2-3")
-    axs["bond_lengths"].legend()
-    plt.show()
+    if energy_plot_file:
+        plt.title("Energy")
+        plt.plot(total_energy, label="Total Energy")
+        plt.plot(potential_energy, label="Potential Energy")
+        plt.plot(kinetic_energy, label="Kinetic Energy")
+        plt.legend()
+        plt.savefig(energy_plot_file)
 
 if __name__ == "__main__":
-    minimize_energy(system)
-    dynamics(system, 50000)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", type=str)
+    parser.add_argument("output_file", type=str)
+    parser.add_argument("-t", "--temperature", type=float, default=300.0)
+    parser.add_argument("-it", "--iterations", type=int, default=1000)
+    parser.add_argument("-plt", "--save-energy-plot", type=str)
+    parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument("-m", "--minimize-energy", action="store_true")
+    args = parser.parse_args()
+
+    infile_path = pathlib.Path(args.input_file)
+
+    if infile_path.suffix == ".py":
+        # https://stackoverflow.com/questions/67631/how-can-i-import-a-module-dynamically-given-the-full-path
+        spec = importlib.util.spec_from_file_location(infile_path.stem, str(infile_path))
+        assert spec is not None
+        py_molecule = importlib.util.module_from_spec(spec)
+        assert spec.loader is not None
+        spec.loader.exec_module(py_molecule)
+
+        try:
+            atoms = py_molecule.atoms
+            connections = py_molecule.connections
+            force_field = py_molecule.force_field
+        except AttributeError:
+            print("Input file must define 'atoms', 'connections', and 'force_field' variables")
+            exit(1)
+    elif infile_path.suffix == ".pdb":
+        raise NotImplementedError("PDB file parsing not implemented yet")
+    else:
+        print(f"Unknown input file format: '${args.input_file.split('.')[-1]}'")
+
+    
+    system = System(atoms, connections, force_field, temperature=args.temperature)
+
+    if args.minimize_energy:
+        minimize_energy(system)
+    
+    run_dynamics(system, args.iterations, args.output_file, verbose=args.verbose, energy_plot_file=args.save_energy_plot)
