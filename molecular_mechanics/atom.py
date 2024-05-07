ethane test molecule, atom testing, fix dihedral angle calculation bug

molecular_mechanics/atom.py
@@ -23,17 +23,18 @@ def get_distance(atom1: Atom, atom2: Atom) -> Tensor:
 
 def get_dihedral_angle(atom1: Atom, atom2: Atom, atom3: Atom, atom4: Atom) -> Tensor:
     # https://math.stackexchange.com/questions/47059/how-do-i-calculate-a-dihedral-angle-given-cartesian-coordinates
-    b1 = atom1.position - atom2.position
+    b1 = atom2.position - atom1.position
     b2 = atom3.position - atom2.position
     b3 = atom4.position - atom3.position
-    b1_normed = b1 / torch.linalg.norm(b1)
-    b2_normed = b2 / torch.linalg.norm(b2)
-    b3_normed = b3 / torch.linalg.norm(b3)
 
-    n1 = torch.linalg.cross(b1_normed, b2_normed)
-    n2 = torch.linalg.cross(b2_normed, b3_normed)
+    cross12 = torch.cross(b1, b2)
+    cross23 = torch.cross(b2, b3)
 
-    m1 = torch.linalg.cross(n1, b2)
+    n1 = cross12 / torch.linalg.norm(cross12)
+    n2 = cross23 / torch.linalg.norm(cross23)
+
+    b2_normalized = b2 / torch.linalg.norm(b2)
+    m1 = torch.linalg.cross(n1, b2_normalized)
 
     x = torch.dot(n1, n2)
     y = torch.dot(m1, n2)

molecular_mechanics/tests/test_atom.py
@@ -1,11 +1,22 @@
 import torch
-from pytest import approx
 
-from molecular_mechanics.atom import Atom, get_bond_angle
+from molecular_mechanics.atom import get_bond_angle, get_dihedral_angle, get_distance
 
+def test_bond_angle(ethane):
+    atoms, _, _ = ethane
+    # H-C-H
+    torch.testing.assert_close(get_bond_angle(atoms[1], atoms[0], atoms[2]), torch.tensor(1.8715999))
+    # H-C-C
+    torch.testing.assert_close(get_bond_angle(atoms[1], atoms[0], atoms[4]), torch.tensor(1.9482204))
 
-def test_bond_angle():
-    atom1 = Atom("H", torch.tensor([0.0, -1.0, 0.0]), 1.0)
-    atom2 = Atom("O", torch.tensor([2.0, 0.0, 0.0]), 16.0)
-    atom3 = Atom("H", torch.tensor([0.0, 0.0, 2.0]), 1.0)
-    assert get_bond_angle(atom1, atom2, atom3) == approx(0.8860771238)
+def test_get_distance(ethane):
+    atoms, _, _ = ethane
+    # H-C
+    torch.testing.assert_close(get_distance(atoms[0], atoms[1]), torch.tensor(1.097788))
+    # C-C
+    torch.testing.assert_close(get_distance(atoms[0], atoms[4]), torch.tensor(1.504454))
+
+def test_dihedral_angle(ethane):
+    atoms, _, _ = ethane
+    # H-C-C-H
+    torch.testing.assert_close(get_dihedral_angle(atoms[1], atoms[0], atoms[4], atoms[5]), torch.tensor(1.0478654))