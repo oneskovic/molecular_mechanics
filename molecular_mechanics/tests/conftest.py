c0c96b23e3080ad84fa91ed9bd76d5dd30bba7fe
Pavle Sekesan <pavlesekesan@gmail.com>
2024-05-08 22:21:43 +0200

    nonbonded forces testing

--- molecular_mechanics/tests/conftest.py
+++ molecular_mechanics/tests/conftest.py
@@ -2,7 +2,7 @@ import torch
 import pytest
 
 from molecular_mechanics.atom import Atom
-from molecular_mechanics.forces import DihedralForce, DihedralForceParams, ForceField, HarmonicAngleForce, HarmonicAngleForceParams, HarmonicBondForce, HarmonicBondForceParams
+from molecular_mechanics.forces import CoulombForce, DihedralForce, DihedralForceParams, ForceField, HarmonicAngleForce, HarmonicAngleForceParams, HarmonicBondForce, HarmonicBondForceParams, LennardJonesForce, LennardJonesForceParams
 
 @pytest.fixture
 def ethane():
@@ -42,4 +42,36 @@ def ethane():
         )
     )
     connections = [[0], [1, 2, 3, 4], [0], [0], [0, 5, 6, 7], [4], [4], [4]]
+    return atoms, force_field, connections
+
+@pytest.fixture
+def three_waters():
+    atoms = [
+        Atom("H", torch.tensor([-0.180, 1.484, 1.180], requires_grad=True), 1.007947),
+        Atom("O", torch.tensor([-0.648, 2.296, 0.824], requires_grad=True), 15.99943),
+        Atom("H", torch.tensor([-1.592, 2.304, 1.156], requires_grad=True), 1.007947),
+        Atom("H", torch.tensor([-3.268, 2.344, 2.736], requires_grad=True), 1.007947),
+        Atom("O", torch.tensor([-3.264, 2.320, 1.736], requires_grad=True), 15.99943),
+        Atom("H", torch.tensor([-3.736, 1.496, 1.420], requires_grad=True), 1.007947),
+        Atom("H", torch.tensor([0.640, 0.076, 2.804], requires_grad=True), 1.007947),
+        Atom("O", torch.tensor([0.644, 0.056, 1.804], requires_grad=True), 15.99943),
+        Atom("H", torch.tensor([0.176, -0.768, 1.488], requires_grad=True), 1.007947),
+    ]
+    force_field = ForceField(
+        harmonic_bond_forces=HarmonicBondForce({
+            ("H", "O"): HarmonicBondForceParams(0.9572, 462.7504),
+        }),
+        harmonic_angle_forces=HarmonicAngleForce({
+            ("H", "O", "H"): HarmonicAngleForceParams(1.82421813418, 836.8),
+        }),
+        lennard_jones_forces=LennardJonesForce({
+            "O": LennardJonesForceParams(0.635968, 0.31507524065751241),
+            "H": LennardJonesForceParams(0.0, 1.0)
+        }),
+        coulomb_forces=CoulombForce({
+            "O": -0.834,
+            "H": 0.417
+        })
+    )
+    connections = [[1], [0, 2], [1], [4], [3, 5], [4], [7], [6, 8], [7]]
     return atoms, force_field, connections
--- molecular_mechanics/tests/test_forces.py
+++ molecular_mechanics/tests/test_forces.py
@@ -2,6 +2,8 @@ import pytest
 import torch
 from torch.testing import assert_close
 
+from molecular_mechanics.constants import COULOMB
+
 def test_bond_force(ethane):
     atoms, forcefield, _ = ethane
     bond_forces = forcefield.harmonic_bond_forces
@@ -11,6 +13,14 @@ def test_bond_force(ethane):
     
     force_cc = bond_forces.get_force(atoms[0], atoms[4])
     assert_close(force_cc, torch.tensor(0.137049))
+
+def test_bond_force_symmetry(ethane):
+    atoms, forcefield, _ = ethane
+    bond_forces = forcefield.harmonic_bond_forces
+
+    force_12 = bond_forces.get_force(atoms[0], atoms[1])
+    force_21 = bond_forces.get_force(atoms[1], atoms[0])
+    assert_close(force_12, force_21)
 
 def test_bond_force_not_found(ethane):
     atoms, forcefield, _ = ethane
@@ -29,6 +39,14 @@ def test_angle_force(ethane):
     force_hcc = angle_forces.get_force(atoms[1], atoms[0], atoms[4])
     assert_close(force_hcc, torch.tensor(0.254513))
 
+def test_angle_force_symmetry(ethane):
+    atoms, forcefield, _ = ethane
+    angle_forces = forcefield.harmonic_angle_forces
+
+    force_hch = angle_forces.get_force(atoms[1], atoms[0], atoms[2])
+    force_hhc = angle_forces.get_force(atoms[2], atoms[0], atoms[1])
+    assert_close(force_hch, force_hhc)
+
 def test_angle_force_not_found(ethane):
     atoms, forcefield, _ = ethane
     angle_forces = forcefield.harmonic_angle_forces
@@ -42,10 +60,54 @@ def test_dihedral_force(ethane):
     force_hcch = dihedral_forces.get_force(atoms[1], atoms[0], atoms[4], atoms[5])
     assert_close(force_hcch, torch.tensor(0.000001))
 
+def test_dihedral_force_symmetry(ethane):
+    atoms, forcefield, _ = ethane
+    dihedral_forces = forcefield.dihedral_forces
+
+    force_hcch = dihedral_forces.get_force(atoms[1], atoms[0], atoms[4], atoms[5])
+    force_hcch_sym = dihedral_forces.get_force(atoms[5], atoms[4], atoms[0], atoms[1])
+    assert_close(force_hcch, force_hcch_sym)
+
 def test_dihedral_force_not_found(ethane):
     atoms, forcefield, _ = ethane
     dihedral_forces = forcefield.dihedral_forces
     with pytest.raises(KeyError):
         dihedral_forces.get_force(atoms[0], atoms[1], atoms[2], atoms[3])
 
-# TODO: Add tests for non-bonded forces
+def test_lennard_jones_force(three_waters):
+    atoms, forcefield, _ = three_waters
+    lj_forces = forcefield.lennard_jones_forces
+
+    # H has epsilon 0 so the force should be 0
+    force_oh = lj_forces.get_force(atoms[0], atoms[4])
+    assert_close(force_oh, torch.tensor(0.0))
+    force_hh = lj_forces.get_force(atoms[0], atoms[3])
+    assert_close(force_hh, torch.tensor(0.0))
+
+    # TODO: The force is very small between O-O, should
+    # test this with a different positions
+    force_oo = lj_forces.get_force(atoms[0], atoms[3])
+    assert_close(force_oo, torch.tensor(0.000005))
+
+def test_lennard_jones_force_symmetry(three_waters):
+    atoms, forcefield, _ = three_waters
+    lj_forces = forcefield.lennard_jones_forces
+
+    force_oh = lj_forces.get_force(atoms[0], atoms[4])
+    force_ho = lj_forces.get_force(atoms[4], atoms[0])
+    assert_close(force_oh, force_ho)
+
+def test_coulomb_force(three_waters):
+    atoms, forcefield, _ = three_waters
+    coulomb_forces = forcefield.coulomb_forces
+
+    force_oh = coulomb_forces.get_force(atoms[2], atoms[4])
+    assert_close(force_oh, torch.tensor(-0.196505*COULOMB))
+
+def test_coulomb_force_symmetry(three_waters):
+    atoms, forcefield, _ = three_waters
+    coulomb_forces = forcefield.coulomb_forces
+
+    force_oh = coulomb_forces.get_force(atoms[2], atoms[4])
+    force_ho = coulomb_forces.get_force(atoms[4], atoms[2])
+    assert_close(force_oh, force_ho)
