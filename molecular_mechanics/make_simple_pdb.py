0681c41a97c085081b1cc9ba0c9ad7ee7c01a4d4
Ognjen <oneskovic0@gmail.com>
2024-05-09 22:14:45 +0200

    Add water pdb files and simple protein generator

---
+++ molecular_mechanics/make_simple_pdb.py
@@ -0,0 +1,25 @@
+"""
+Simple example script demonstrating how to use the PeptideBuilder library.
+
+The script generates a peptide consisting of six arginines in alpha-helix
+conformation, and it stores the peptide under the name "example.pdb".
+"""
+
+from PeptideBuilder import Geometry
+import PeptideBuilder
+
+# create a peptide consisting of 3 glycines
+geo = Geometry.geometry("G")
+geo.phi = -60
+geo.psi_im1 = -40
+structure = PeptideBuilder.initialize_res(geo)
+for i in range(2):
+    PeptideBuilder.add_residue(structure, geo)
+# add terminal oxygen (OXT) to the final glycine
+PeptideBuilder.add_terminal_OXT(structure)
+
+import Bio.PDB
+
+out = Bio.PDB.PDBIO()
+out.set_structure(structure)
+out.save("example.pdb")
